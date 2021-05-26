831a3077585eb502897823843b34fb23ddf49083
Eldorbek Makhkamov <79107463+makhweb@users.noreply.github.com>
2021-05-26 22:17:47 +0500

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
+  requireHeader: [],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
